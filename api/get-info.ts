120debe53c302f1c3d982f6d050429dd30ee2bfe
Maxim Stepanov <stepanov.maxim-dev@yandex.ua>
2019-11-18 11:57:28 +0200

    add app config

--- api/get-info.ts
+++ api/get-info.ts
@@ -1,9 +1,19 @@
 import { NowRequest, NowResponse } from '@now/node'
-import * as ytdl from 'ytdl-core';
 import {NowRequestQuery} from "@now/node/dist";
+import * as ytdl from 'ytdl-core';
 
 export default async (req: NowRequest, res: NowResponse) => {
-    const { url = '' }: NowRequestQuery = req.query;
-    const info = await ytdl.getInfo(String(url));
-    res.json(info)
+    try {
+        const { url = '' }: NowRequestQuery = req.query;
+        if (!url) {
+            return res.status(422).json({
+                error: 'Url is required'
+            })
+        }
+        const info = await ytdl.getInfo(String(url));
+        res.json(info)
+    } catch (e) {
+        return e;
+    }
+
 }
---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import terminal from "chalk-animation";
+
+// api
+import getInfo from './api/get-info';
+
+const app = express();
+
+const router = express.Router();
+
+router.use('/get-info', getInfo);
+
+router.use('/',  (req, res) => {
+    res.send('It"s works')
+});
+
+app.use('/api', router);
+
+app.listen(3000, () => {
+    terminal.karaoke('server listening on http://127.0.0.1:' + 3000);
+});
